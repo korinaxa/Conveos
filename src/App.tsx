68e11f38a50927ef83a7726caefc976660cff887
Kyriaki Charalampi <csd3731@csd.uoc.gr>
2023-06-20 19:51:39 +0300

    Movies or series displayed at page

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,15 @@ interface Movie {
 
 const App: React.FC = () => {
   const [selectedOption, setSelectedOption] = useState<string>("");
+  const [searchResults, setSearchResults] = useState<Movie[]>([]);
 
   const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedOption(event.target.value);
   };
 
-  const handleSearch = () => {
-    axios.get(`http://www.omdbapi.com/?apikey=9e3139c8&s=${selectedOption}`).then((response) => {
-        console.log(response.data);
+  const handleSearch = () => {  
+    axios.get(`http://www.omdbapi.com/?apikey=9e3139c8&s=${selectedOption}&type=${selectedOption}`).then((response) => {
+      setSearchResults(response.data.Search);
       })
       .catch((error) => {
         console.error(error);
@@ -29,7 +30,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
         <label>
-          <input type="radio" value="movie" checked={selectedOption === "movie"} onChange={handleOptionChange}/>
+          <input type="radio" value="movie" checked={selectedOption === "movie"} onChange={handleOptionChange} defaultChecked/>
           Movies
         </label>
         <label>
@@ -37,6 +38,17 @@ const App: React.FC = () => {
           Series
         </label>
       <button onClick={handleSearch}>Search</button>
+      
+      <div>
+        {searchResults.map((movie) => (
+          <div className="my" key={movie.imdbID}>
+            <h3>{movie.Title}</h3>
+            <p>Year: {movie.Year}</p>
+            <img src={movie.Poster} alt={movie.Title} />
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 };
