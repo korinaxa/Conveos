fa4818f473dbf4e39aac04d20cf9f6e2c4502c71
Kyriaki Charalampi <csd3731@csd.uoc.gr>
2023-06-20 18:52:02 +0300

    Getting movies and series with api request and displays them at the console

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from "react";
+import axios from "axios";
 import './App.css';
 
-function App() {
+interface Movie {
+  Title: string;
+  Year: string;
+  imdbID: string;
+  Type: string;
+  Poster: string;
+}
+
+const App: React.FC = () => {
+  const [selectedOption, setSelectedOption] = useState<string>("");
+
+  const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const handleSearch = () => {
+    axios.get(`http://www.omdbapi.com/?apikey=9e3139c8&s=${selectedOption}`).then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <div className="App">
-        <input type="radio" id="movies" name="type" value="movies" defaultChecked/>MOVIES
-        <input type="radio" id="series" name="type" value="series"/>SERIES
+        <label>
+          <input type="radio" value="movie" checked={selectedOption === "movie"} onChange={handleOptionChange}/>
+          Movies
+        </label>
+        <label>
+          <input type="radio" value="series" checked={selectedOption === "series"} onChange={handleOptionChange}/>
+          Series
+        </label>
+      <button onClick={handleSearch}>Search</button>
     </div>
   );
-}
+};
 
 export default App;
