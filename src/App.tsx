6c81a141a5649722ba4dee50c5b7d98f5fbec5d5
Kyriaki Charalampi <csd3731@csd.uoc.gr>
2023-06-20 20:30:40 +0300

    Search button removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import './App.css';
 
@@ -11,37 +11,46 @@ interface Movie {
 }
 
 const App: React.FC = () => {
-  const [selectedOption, setSelectedOption] = useState<string>("");
+  const [selectedOption, setSelectedOption] = useState<string>("movie");
   const [searchResults, setSearchResults] = useState<Movie[]>([]);
+
+
+  useEffect(() => {
+    if (selectedOption === "") {
+      setSearchResults([]);
+      return;
+    }
+
+    axios
+      .get(
+        `http://www.omdbapi.com/?apikey=9e3139c8&s=${selectedOption}&type=${selectedOption}`
+      )
+      .then((response) => {
+        setSearchResults(response.data.Search);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [selectedOption]);
 
   const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedOption(event.target.value);
   };
 
-  const handleSearch = () => {  
-    axios.get(`http://www.omdbapi.com/?apikey=9e3139c8&s=${selectedOption}&type=${selectedOption}`).then((response) => {
-      setSearchResults(response.data.Search);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-
   return (
     <div className="App">
-        <label>
-          <input type="radio" value="movie" checked={selectedOption === "movie"} onChange={handleOptionChange} defaultChecked/>
+        <label id="label_movies">
+          <input type="radio" value="movie" checked={selectedOption === "movie"} onChange={handleOptionChange}/>
           Movies
         </label>
-        <label>
+        <label id="label_series">
           <input type="radio" value="series" checked={selectedOption === "series"} onChange={handleOptionChange}/>
           Series
         </label>
-      <button onClick={handleSearch}>Search</button>
       
-      <div>
+      <div className="all_objects">
         {searchResults.map((movie) => (
-          <div className="my" key={movie.imdbID}>
+          <div className="object_container" key={movie.imdbID}>
             <h3>{movie.Title}</h3>
             <p>Year: {movie.Year}</p>
             <img src={movie.Poster} alt={movie.Title} />
